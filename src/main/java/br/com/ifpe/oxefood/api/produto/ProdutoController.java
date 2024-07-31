12fb76e280fdf98ab217720c0ed5cf4587a036ca
aluno <aluno@ifpe>
2024-07-31 20:10:28 -0300

    quase

--- src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
+++ src/main/java/br/com/ifpe/oxefood/api/produto/ProdutoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ifpe.oxefood.modelo.produto.Produto;
@@ -41,6 +42,15 @@ public class ProdutoController {
     @GetMapping("/{id}")
     public Produto obterPorID(@PathVariable Long id) {
         return produtoService.obterPorID(id);
+    }
+
+    @PostMapping("/filtrar")
+    public List<Produto> filtrar(
+            @RequestParam(value = "codigo", required = false) String codigo,
+            @RequestParam(value = "titulo", required = false) String titulo,
+            @RequestParam(value = "idCategoria", required = false) Long idCategoria) {
+
+        return produtoService.filtrar(codigo, titulo, idCategoria);
     }
 
     @PutMapping("/{id}")

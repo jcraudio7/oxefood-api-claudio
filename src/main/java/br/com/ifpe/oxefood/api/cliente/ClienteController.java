dee42b295da90d813058738c49f8a46967ac411c
aluno <aluno@ifpe>
2024-08-01 19:36:20 -0300

    exe 19

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,5 +1,6 @@
 package br.com.ifpe.oxefood.api.cliente;
 
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,59 +18,72 @@ import java.util.List;
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin
 public class ClienteController {
-   
+
     @Autowired
-   private ClienteService clienteService;
+    private ClienteService clienteService;
 
-@Operation(
-       summary = "Serviço responsável por salvar um cliente no sistema."    
-   )
+    @Operation(summary = "Serviço responsável por salvar um cliente no sistema.")
 
-   @PostMapping
-   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
+    @PostMapping
+public ResponseEntity<Cliente> save(@RequestBody @Valid ClienteRequest request) {
 
-       Cliente cliente = clienteService.save(request.build());
-       return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
-   }
-  
-   @Operation(
-    summary = "Serviço responsável por listar um cliente no sistema."
-)
-     @GetMapping
+	Cliente clienteRequisicao = request.buildCliente();
+	StringBuilder erros = new StringBuilder(); 
+		
+	//O campo nome não pode ser nulo e nem vazio
+	if (clienteRequisicao.getNome() == null || clienteRequisicao.getNome().equals("")) {
+	    erros.append("O campo Nome é de preenchimento obrigatório. ");
+	}
+	
+	//O campo nome não pode ser maior que 100 caracteres
+	...
+	
+	
+	//O campo fone tem que ter mais que 8 caracteres e menos que 20
+	if (clienteRequisicao.getFone() != null && (clienteRequisicao.getFone().length() < 8 || clienteRequisicao.getFone().length() > 20)) {
+	    erros.append("O campo Fone tem que ter entre 8 e 20 caracteres. ");
+	}
+	
+	if (erros.length() > 0) {
+	    throw new BadRequestException(erros.toString());
+	}
+	
+	Cliente clienteSalvo = clienteService.save(clienteRequisicao);
+	return new ResponseEntity<Cliente>(clienteSalvo, HttpStatus.CREATED);
+}
+
+    @GetMapping
     public List<Cliente> listarTodos() {
         return clienteService.listarTodos();
     }
-    
+
     @GetMapping("/{id}")
     public Cliente obterPorID(@PathVariable Long id) {
         return clienteService.obterPorID(id);
     }
-    
-    @Operation(
-    summary = "Serviço responsável por alterar um cliente no sistema."    
-   )
-    
-   @PutMapping("/{id}")
+
+    @Operation(summary = "Serviço responsável por alterar um cliente no sistema.")
+
+    @PutMapping("/{id}")
     public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
 
-       clienteService.update(id, request.build());
-       return ResponseEntity.ok().build();
- }
-  @Operation(
-    summary = "Serviço responsável por deletar um cliente no sistema."    
-   )
+        clienteService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
 
-   @DeleteMapping("/{id}")
-   public ResponseEntity<Void> delete(@PathVariable Long id) {
+    @Operation(summary = "Serviço responsável por deletar um cliente no sistema.")
 
-       clienteService.delete(id);
-       return ResponseEntity.ok().build();
-   }
-   
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        clienteService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/Cliente.java
@@ -6,6 +6,8 @@ import org.hibernate.annotations.SQLRestriction;
 import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,20 +23,24 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cliente extends EntidadeAuditavel{
-    @Column
+public class Cliente extends EntidadeAuditavel {
+
+    @OneToMany(mappedBy = "cliente", orphanRemoval = true, fetch = FetchType.EAGER)
+    private List<EnderecoCliente> enderecos;
+
+    @Column(nullable = false, length = 100)
     private String nome;
 
     @Column
-   private LocalDate dataNascimento;
+    private LocalDate dataNascimento;
 
-   @Column
-   private String cpf;
+    @Column(unique = true)
+    private String cpf;
 
-   @Column
-   private String foneCelular;
+    @Column
+    private String foneCelular;
 
-   @Column
-   private String foneFixo;
+    @Column
+    private String foneFixo;
 
 }
